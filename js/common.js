08e37c9ac350ae9dcbb20ff8a9382ea4b54281c2
Ratibor <klov29@mail.ru>
2018-01-11 15:21:53 +0700

    Сделал меню на главной

--- js/common.js
+++ js/common.js
@@ -1,4 +1,22 @@
 "use strict";
 
-$(function () {});
+$(function () {
+
+    /* Гамбургер
+  =========================*/
+    $(".menu_btn").click(function() {
+        var elem = $(this);
+        elem.toggleClass("menu_btn_active");
+        if(elem.hasClass("menu_btn_active")) {
+            $(".header__menu-mob").slideDown(500);
+        } else {
+            $(".header__menu-mob").slideUp(500);
+        }
+    });
+
+
+
+
+
+});
 
