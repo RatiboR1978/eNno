990f83c8b11114f43acfb71fa9d504e613432efd
Ratibor <klov29@mail.ru>
2018-01-12 13:55:31 +0700

    Прицепил слайдер и сделал раздел theme

--- gulpfile.js
+++ gulpfile.js
@@ -36,6 +36,7 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 gulp.task('scripts', function() {
 	return gulp.src([ // Берем все необходимые библиотеки
 		'libs/jquery/dist/jquery.min.js', // Берем jQuery
+        'libs/slick.js', // Берем slick
 		'libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
 		])
 		.pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
--- js/common.js
+++ js/common.js
@@ -4,14 +4,25 @@ $(function () {
 
     /* Гамбургер
   =========================*/
-    $(".menu_btn").click(function() {
+    $('.menu_btn').click(function() {
         var elem = $(this);
-        elem.toggleClass("menu_btn_active");
-        if(elem.hasClass("menu_btn_active")) {
-            $(".header__menu-mob").slideDown(500);
+        elem.toggleClass('menu_btn_active');
+        if(elem.hasClass('menu_btn_active')) {
+            $('.header__menu-mob').slideDown(500);
         } else {
-            $(".header__menu-mob").slideUp(500);
+            $('.header__menu-mob').slideUp(500);
         }
+    });
+
+    /* slick
+  =========================*/
+    $('.slider-items').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000
     });
 
 
